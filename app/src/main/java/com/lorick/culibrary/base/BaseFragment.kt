dabe861f8e64fa026b1656537d2ba28a1e9e3d53
Lorick <LorickMr@gmail.com>
2024-03-20 00:16:22 +0530

    Project setup Commit by Lorick

---
+++ app/src/main/java/com/lorick/culibrary/base/BaseFragment.kt
@@ -0,0 +1,37 @@
+package com.lorick.culibrary.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+
+abstract class BaseFragment<VB : ViewBinding>(
+    private val layoutInflater: (inflater: LayoutInflater) -> VB
+) : Fragment() {
+
+    private lateinit var viewBinding: VB
+    val binding: VB
+        get() = viewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewBinding = layoutInflater.invoke(inflater)
+        return viewBinding.root
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+//        hideKeyboard(requireActivity())
+    }
+}
